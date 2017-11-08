d08479240d59ce1b41f4451fd912b74cf04dca55
joselledinana <33414100+joselledinana@users.noreply.github.com>
2017-11-08 11:00:41 +0200

    Add files via upload

---
+++ MainEmpCls.java
@@ -0,0 +1,28 @@
+/**
+ * @(#)MainEmpCls.java
+ *
+ *
+ * @author
+ * @version 1.00 2016/9/26
+ */
+import java.util.*;
+
+public class MainEmpCls {
+
+   static Scanner con= new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+       EmpCls myEmpCls = new EmpCls();
+       String name;
+       String ext;
+       int yearStarted;
+
+      myEmpCls.setEmpDetails("Joselle","x500",2016);
+
+      System.out.println(" Name:"+name);
+      System.out.println(" Ext:" +ext);
+      System.out.println(" Year Started:"+ yearStarted);
+    }
+
+}
