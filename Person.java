044a41660296c88982721bc0bf77c1668cc35bf3
Koumou <33414897+Koumou@users.noreply.github.com>
2017-11-09 10:58:25 +0200

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,44 @@
+
+public class Person {
+
+public class Person {
+
+    private String name;
+    private int age;
+
+    public Person(String name, int age)
+    {
+    	this.name=name;
+        this.age=age;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setAge(int age){
+        this.age=age;
+    }
+    public int getAge(){
+        return age;
+    }
+
+
+
+    public boolean equals(Object e) {
+        if (this == e) return true;
+        if (e == null || getClass() != o.getClass()) return false;
+
+        Person per = (Person) o;
+
+        return age == per.age;
+    }
+
+
+    public int hashCode() {
+        return age;
+    }
+
+}
